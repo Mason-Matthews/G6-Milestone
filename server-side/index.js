0c504b71773674bf62712181e164771659f88735
Mason-Matthews <masonmatthews98@gmail.com>
2022-08-04 20:35:09 -0700

    connected database

--- server-side/index.js
+++ server-side/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { connect } = require('react-redux');
+const { Schema } = mongoose;
+
 
 require('dotenv').config();
 const PORT = process.env.PORT;
@@ -14,14 +16,25 @@ mongoose.connect(process.env.MONGO_URI,{
     console.log(`connected to mongo: ${process.env.MONGO_URI}`);
 });
 
+const expenseSchema = new Schema({
+    name: {type: String},
+    type: {type: String},
+    amount: {type: Number},
+})
+
+const expense1 = mongoose.model("expense1", expenseSchema);
+
+app.get("/",(req, reg) =>{
+    reg.send();
+    
+    console.log(expense1)
+    
+    expense1.find().then((foundExpense) => {
+        console.log(foundExpense);
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
-//comment
-
-// require('dotenv').config()
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
-
 
