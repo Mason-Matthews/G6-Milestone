c84206a743fbb6f13eeed154533b2cd5b911e25d
Mason-Matthews <masonmatthews98@gmail.com>
2022-08-11 16:41:41 -0700

    heroku push

--- server-side/index.js
+++ server-side/index.js
@@ -1,13 +1,13 @@
 // DEPENDENCIES
 const express = require("express");
 const cors = require("cors");
-
+const path = require('path');
 
 require("dotenv").config();
 const mongoose = require("mongoose");
 
 // CONFIGURATION
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
@@ -36,6 +36,9 @@ const expenseSchema ={
 const expense = mongoose.model("expense", expenseSchema);
 module.exports = expense;
 
+//build
+app.use(express.static(path.resolve(__dirname, '../client-side/build')));
+
 // ROUTES
 app.get("/", (req, res) => {
   console.log(req.body)
@@ -59,45 +62,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-side/build', 'index.html'));
+});
 // LISTEN
 app.listen(3001, () => {
   console.log("New Greetings! Server listening From port: ", 3001);
 });
-
-
-// mongoose.connect(process.env.MONGO_URI,{
-//     useNewUrlParser: true,
-//     UseUnifiedTopology: true,
-// },
-// () => {
-//     console.log(`connected to mongo: ${process.env.MONGO_URI}`);
-// });
-
-// const expenseSchema = new Schema({
-//     amount: {type: Number},
-//     name: {type: String},
-//     type: {type: String},
-//     date: {type: Date},
-// })
-
-// const expense1 = mongoose.model("expense1", expenseSchema);
-
-// app.get("/",(req, res) =>{
-   
-    
-//     console.log(expense1)
-    
-//     expense1.find().then((foundExpense) => {
-//         console.log(foundExpense);
-//     })
-// });
-
-// app.get('/api', (req, res) => {
-//     res.json({ message: 'Hello from server!' });
-// });
-// //app.use(express.static(path.resolve(__dirname, "../client-side/src/App.js" )));
-
-// app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-//   });
-
